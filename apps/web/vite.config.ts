c9b33579494375b635e2da808ed47d989a1fa267
wtaanu <wtaanu@gmail.com>
2026-04-22 19:11:51 +0530

    viteconfig

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: ["app.reshamsutra.com"]
+  },
+  preview: {
+    allowedHosts: ["app.reshamsutra.com"]
+  }
 });
-
